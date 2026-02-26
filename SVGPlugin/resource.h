f75ec8d84a303fb2097479fc81a78a8b08d333c3
Bob Voith <bobvoith@gmail.com>
2026-02-26 16:53:50 +0100

    Updated documentation and third-party licenses

--- SVGPlugin/resource.h
+++ SVGPlugin/resource.h
@@ -6,29 +6,25 @@
 // SVG Plugin (MysticThumbs) configuration dialog
 #define IDD_SVG_PLUGIN_CONFIGURE          101
 
-#define IDC_CFG_LOG_ENABLED               2001
-#define IDC_CFG_LOG_INCLUDE_CRC           2002
-#define IDC_CFG_LOG_FILENAME_LBL          2003
-#define IDC_CFG_LOG_FILENAME              2004
-#define IDC_CFG_LOG_BROWSE                2005
+#define IDC_CFG_SWAP_RB                   2001
+#define IDC_CFG_RETURN_DEBUG              2002
+#define IDC_CFG_USE_DESIRED_SIZE_HINT     2003
+#define IDC_CFG_LEAVE_TEMP                2004
 
-#define IDC_CFG_SWAP_RB                   2011
-#define IDC_CFG_RETURN_DEBUG              2012
-#define IDC_CFG_USE_DESIRED_SIZE_HINT     2013
-#define IDC_CFG_MAX_DIM                   2020
-#define IDC_CFG_MAX_BYTES                 2021
-#define IDC_CFG_LEAVE_TEMP                2010
+#define IDC_CFG_MAX_DIM                   2010
+#define IDC_CFG_MAX_BYTES                 2011
 
-#define IDC_CFG_NORM_ROOT_FILL_WHITE      2030
-#define IDC_CFG_NORM_RGBA_ATTR            2031
-#define IDC_CFG_NORM_RGBA_STYLE           2032
 
-#define IDC_CFG_THUMB_ENABLE              2041
-#define IDC_CFG_THUMB_PATH                2042
-#define IDC_CFG_THUMB_PATH_LBL            2043
-#define IDC_CFG_THUMB_BROWSE              2044
-#define IDC_CFG_THUMB_PARAMS              2045
-#define IDC_CFG_THUMB_PARAMS_LBL          2046
+#define IDC_CFG_NORM_ROOT_FILL_WHITE      2020
+#define IDC_CFG_NORM_RGBA_ATTR            2021
+#define IDC_CFG_NORM_RGBA_STYLE           2022
+
+#define IDC_CFG_THUMB_ENABLE              2031
+#define IDC_CFG_THUMB_PATH                2032
+#define IDC_CFG_THUMB_PATH_LBL            2033
+#define IDC_CFG_THUMB_BROWSE              2034
+#define IDC_CFG_THUMB_PARAMS              2035
+#define IDC_CFG_THUMB_PARAMS_LBL          2036
 
 
 // Next default values for new objects
@@ -37,7 +33,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        104
 #define _APS_NEXT_COMMAND_VALUE         40001
-#define _APS_NEXT_CONTROL_VALUE         2047
+#define _APS_NEXT_CONTROL_VALUE         2037
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
